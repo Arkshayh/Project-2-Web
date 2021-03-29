e7ed2a4007608b06b7efed1daa122056f8e8bfda
55019 <55019@etu.he2b.be>
2021-03-29 13:22:17 +0200

    display image + question + reponse + button verification

--- javascript/pageQuiz.js
+++ javascript/pageQuiz.js
@@ -11,11 +11,33 @@ function displayQuestionInConsole(tabThem) {
 
 function displayQuizz() {
     let tabThem = quizzes[valeur].data; 
+    let tabRep;
+    let val;
+
     displayQuestionInConsole(tabThem); //Affichage question dans console
 
     $("main").append('<div id="QuestionReponse"></div>');
 
-    $("#QuestionReponse").append('<img src="'+tabThem[0].image+'"alt="image">')
+    for(let i = 0; i < tabThem.length; i++){
+
+        val = i + 1;
+
+        $("#QuestionReponse").append('<img src="'+tabThem[i].image+'"alt="image">');
+        $("#QuestionReponse").append('<div id ="Question'+ val +'"> Question numéro '+ val +' : '+ tabThem[i].question +'</div');
+
+        tabRep = tabThem[i].reponses;
+    
+        for(let j = 0; j < tabRep.length;j++){
+            $("#Question"+val).append('<div class ="Reponse"> <input type="radio" id ="'+ tabRep[j] +'" name="choix'+ i +'" value="'+
+             tabRep[j] +'"> <label for="'+ tabRep[j] +'">'+ tabRep[j] +'</label> </div>');
+        }
+        $("#QuestionReponse").append('<br></br>');
+        
+    }
+
+    $("#QuestionReponse").append('<button id="verification">Vérification</button>')
+
+    
 }
 
 $(document).ready(function () {
