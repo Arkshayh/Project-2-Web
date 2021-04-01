f1f915e915a19a3f1588b42fe2a48a5449fb81c3
55019 <55019@etu.he2b.be>
2021-04-01 14:55:47 +0200

    >Display quizz for multiple answer quizz work

--- javascript/pageQuiz.js
+++ javascript/pageQuiz.js
@@ -1,3 +1,5 @@
+//Tell if the quizz chosen has multiple answer or not
+let multipleAnswer = false;
 let valeur = localStorage.getItem('valeur');
 
 function DisplayTitle(){
@@ -10,9 +12,6 @@ function displayQuestionInConsole(tabThem) {
         console.log(tabThem[i].question);
     }
 }
-
-//Tell if the quizz chosen has multiple answer or not
-let multipleAnswer = false;
 
 //Will check if the quizz chosen has multiple answer if it's the case let multipleAnswer -> true 
 function countAnswer(){
@@ -57,7 +56,21 @@ function displayQuizzUniqueAnswer(tabThem){
 }
 
 function displayQuizzMultipleAnswer(tabThem){
+    let tabRep;
+    let val;
 
+    for(let i = 0; i < tabThem.length; i++){
+        val = i + 1;
+        $("#QuestionReponse").append('<img src="'+tabThem[i].image+'"alt="image">');
+        $("#QuestionReponse").append('<div id ="Question'+ val +'"> Question numéro '+ val +' : '+ tabThem[i].question +'</div');
+        tabRep = tabThem[i].reponses;
+        
+        for(let j = 0; j < tabRep.length;j++){
+            $("#Question"+val).append('<div class ="Reponse"> <input type="checkbox" id ="'+ tabRep[j] +'" name="choix'+ i +'" value="'+
+             j +'"> <label for="'+ tabRep[j] +'">'+ tabRep[j] +'</label> </div>');
+        }
+        $("#QuestionReponse").append('<br>');
+    }
 }
 
 function displayQuizz() {
@@ -74,18 +87,70 @@ function displayQuizz() {
         displayQuizzUniqueAnswer(tabThem);
     }
     else{
-        displayQuizzMultipleAnswer();
+        displayQuizzMultipleAnswer(tabThem);
     }
 
     $("main").append('<button id="verification">Vérification</button>')
     $("#verification").click(function(){
-        let resultat = verification();
-        localStorage.setItem("res", resultat);
-        openResultatPage();
+        if(multipleAnswer == false){
+            let resultat = verification();
+            localStorage.setItem("res", resultat);
+            openResultatPage();
+        }
+        else{
+            let bonneRep = getBonneReponse(tabThem);
+            let enough = checking(bonneRep);
+            if(enough == false){
+                alert("Vous n'avez pas coché assez de case !");
+            }
+            else{
+                let userAnswer = getMultpleAnswer(tabThem);
+                localStorage.setItem("res", userAnswer);
+                openResultatPage();
+            }
+
+            console.log("à faire");
+        }
     })
 
 }
 
+function checking(bonneRep){
+    let enough = true;
+    let count = 0;
+    for(let i = 0; i < bonneRep.length;i++){
+        document.getElementsByName('choix'+i).forEach(checkbox => {
+            if(checkbox.checked){
+                count++;
+            }
+        })
+        if(bonneRep[i].length != count){
+            enough = false;
+        }
+        count = 0;
+    }
+    return enough;
+}
+
+function getMultpleAnswer(tabThem){
+    let checked = [];
+    for(let i =0; i< tabThem.length;i++){
+        document.getElementsByName('choix'+i).forEach(checkbox => {
+            if(checkbox.checked){
+                checked.push(checkbox.value);
+            }
+        })
+    }
+}
+
+function getBonneReponse(tabThem){
+    let bonneRep = [];
+    for(let i =0; i< tabThem.length;i++){
+        bonneRep.push(tabThem[i].bonneReponses);
+    }
+    return bonneRep;
+
+}
 
 function tabRepChecked() {
     let tabRep = [];
